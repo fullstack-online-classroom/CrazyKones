2454125112573482309576d589ada96bda2766aa
Filipe Silva <149911930+filipejosilva@users.noreply.github.com>
2024-02-13 20:46:49 +0000

    ls and mk dir command, bug fixes

--- FTP-Client/src/main/java/Client.java
+++ FTP-Client/src/main/java/Client.java
@@ -4,13 +4,10 @@ import java.net.Socket;
 public class Client {
 
     private Socket clientSocket;
-
     private BufferedReader inputBufferReader;
-
     private BufferedWriter outputBufferWriter;
-
     private PrintWriter out;
-
+    BufferedReader in;
     public Client(){
         try {
             clientSocket = new Socket("127.0.0.1", 9000);
@@ -20,28 +17,31 @@ public class Client {
             throw new RuntimeException("client cannot connect to server");
         }
         String line = "";
-        String serverEcho = "";
+
         try {
             while  (!line.equals("QUIT")){
-//                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-//                System.out.println(in.readLine());
+                //System.out.println(in.readLine());
                 line = inputBufferReader.readLine();
                 outputBufferWriter.write(line);
                 outputBufferWriter.newLine();
                 outputBufferWriter.flush();
-                //reading message from server
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-//                 serverEcho = in.readLine();
-                while ((serverEcho = in.readLine()) != null){
 
-                  if (serverEcho == null) {
-                      break;
-                  }
-                        System.out.println(serverEcho);
-//                 }
+                //reading message from server
+                String serverEcho;
+
+                //exit
+                while ((serverEcho = in.readLine()) != null && !serverEcho.equals("exit")){
+                    if(serverEcho.equals("exit")){
+                        break;
+                    }
+                    System.out.println(serverEcho);
+
                 }
+
+
             }
+            outputBufferWriter.close();
         }
         catch (IOException e){
             throw new RuntimeException("");
@@ -51,7 +51,9 @@ public class Client {
     private void setUpSocketStreams() throws IOException {
         inputBufferReader = new BufferedReader(new InputStreamReader(System.in));
         outputBufferWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-        out = new PrintWriter(clientSocket.getOutputStream());
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
     }
 
     public static void main(String[] args) {
--- FTP-Server/src/main/java/Server.java
+++ FTP-Server/src/main/java/Server.java
@@ -1,6 +1,8 @@
+import javax.management.StringValueExp;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Server {
 
@@ -23,20 +25,61 @@ public class Server {
             System.out.println("Client connected " + clientSocket);
             setUpSocketStreams();
 //            out.println("Server : You're Connected");
+            label:
             while (true){
-                String clientMessage = inputBufferReader.readLine();
-                if(clientMessage.equals("QUIT") || clientMessage.equals("BYE") || clientMessage.equals("DISCONNECT")){
-                    System.out.println("connection terminated");
-                    out.println("connection terminated");
-                    break;
-                } else if (clientMessage.equals("HELP"))
-                {
-                    help();
+                String clientMessage = inputBufferReader.readLine().toUpperCase();
+
+                switch (clientMessage) {
+                    case "QUIT":
+                    case "BYE":
+                    case "DISCONNECT":
+                        System.out.println("connection terminated");
+                        out.println("connection terminated");
+                        break label;
+                    case "HELP":
+                        //out.println("Commands: QUIT, LS, PUT, GET, MKDIR");
+                        help();
+                        break;
+                    case "LS":
+                        out.println(lsCommand());
+                        break;
+                    case "GET":
+                        out.println("Command not available!");
+                        break;
+                    case "PUT":
+                        out.println("Command not available!");
+                        break;
+                    case "MKDIR":
+                        String folder;
+                        while(true){
+                            out.println("Give a name to folder:");
+                            out.println("exit");
+                            folder = inputBufferReader.readLine();
+                            while(folder == ""){
+                                out.println("That not a good name!!Give a better name: ");
+                                out.println("exit");
+                                folder = inputBufferReader.readLine();
+
+                            }
+                            if(folder != null){
+                                break;
+                            }
+                        }
+                        makeDir(folder);
+                        out.println("We created a folder!");
+                        break;
+                    default:
+                        out.println("Command not available");
+                        break;
                 }
                 System.out.println(clientMessage);
-                out.println(clientMessage + " this is message from client");
+                out.println("exit");
+                //out.println(clientMessage + " server receive this message");
+                //out.flush();
 
             }
+            out.close();
+            inputBufferReader.close();
         }
         catch (IOException e){
             throw new RuntimeException("");
@@ -51,8 +94,30 @@ public class Server {
         out.println("get" + " - get a file from the server ");
         out.println("mkdir" + " - create a directory on the server \n");
 
+    }
+    
+    public String lsCommand(){
+        //what the directory
+        File file = new File("serverRoot/");
+        //get all files
+        String [] allFiles = file.list();
 
+        String files = "";
+        for(int i = 0; i < allFiles.length; i++){
 
+            if(i == (allFiles.length -1)){
+                files += allFiles[i];
+                break;
+            }
+            files += allFiles[i] + ", ";
+        }
+        
+        return files;
+    }
+
+    public void makeDir(String folder){
+        File file = new File("serverRoot/" + folder);
+        file.mkdir();
     }
 
 
