41400aa8f8a89620a9c1dd29a6a89217789c96d8
Filipe Silva <149911930+filipejosilva@users.noreply.github.com>
2024-02-17 17:15:49 +0000

    get bug fixed

--- FTP-Client/src/main/java/Client.java
+++ FTP-Client/src/main/java/Client.java
@@ -71,7 +71,8 @@ public class Client {
         try {
 
             //Max byte size
-            byte fileSize[] = new byte[900000000];
+            //byte fileSize[] = new byte[900000000];
+            byte fileSize[] = new byte[clientSocket.getReceiveBufferSize()];
             //receive input
             InputStream is = clientSocket.getInputStream();
 
@@ -81,25 +82,22 @@ public class Client {
             //convert file to bytes
             BufferedOutputStream downloadFile = new BufferedOutputStream(download);
 
-            //gets the bytes, current is used to know the size of the file
-            int bytesRead;
-            int current = 0;
+            //gets the bytesread to know the size
+            int bytesRead ;
+            bytesRead = is.read(fileSize);
 
             //If theres no more bytes to read we get out
-            while (is.available() != 0){
-                //Getting bytes read, start where current is. We start at 0
-                bytesRead = is.read(fileSize, current, (fileSize.length-current));
-                //System.out.println(bytesRead);
-
-                if(bytesRead >= 0){
-                    current += bytesRead;
-                    //System.out.println(current + ":terminal");
+            while (bytesRead != -1){
+                //Writing the file
+                downloadFile.write(fileSize, 0, bytesRead);
+                if(is.available() == 0){
+                    break;
                 }
-                //System.out.println("available " + is.available());
+                bytesRead = is.read(fileSize);
             }
             //Push bytes to the filee
-            downloadFile.write(fileSize, 0, current);
-            downloadFile.flush();
+            //downloadFile.write(fileSize, 0, current);
+            //downloadFile.flush();
             downloadFile.close();
             //System.out.println("terminal: I am out");
 
